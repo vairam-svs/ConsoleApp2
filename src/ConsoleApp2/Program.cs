42ff5366d4a3ade0f66122b60140e8e9877ff44a
vairam-svs <vairam.svs@outlook.com>
2015-05-09 08:59:12 -0700

    Math extensions not working

---
+++ src/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            string inputValue = Console.ReadLine();
+            int splitInterval = 2;
+            int outPutValue = (int)Math.Ceiling(inputValue.Length / (decimal)splitInterval);
+            Console.WriteLine(outPutValue.ToString());
+            Console.ReadLine();
+        }
+    }
+}
